f3ef7559bc77c1449f747759da9d8aa6b4341969
Lars Bergmann <lars.bergmann@perfact.de>
2019-10-10 22:27:57 +0200

    Start to cythonize bh engine

---
+++ cygravity/engine_bh.pyx
@@ -0,0 +1,228 @@
+'''
+Barnes-Hut n-body engine
+'''
+
+import math
+
+
+cdef class Body(object):
+
+    cdef public (float, float) cog
+    cdef public (float, float) vel
+    cdef public int mass
+    cdef public int remove
+    cdef public int collision
+    cdef public float next_force_x
+    cdef public float next_force_y
+
+    def __init__(self, cog, vel, mass):
+        self.cog = cog
+        self.vel = vel
+        self.mass = mass
+        self.collision = False
+        self.remove = False
+        self.next_force_x = 0
+        self.next_force_y = 0
+
+'''
+cdef class Node(object):
+    cdef public (float, float) cog
+    cdef public (float, float) pos
+    cdef public int mass
+    cdef public int size
+    cdef public list children
+    cdef public list bodies
+'''
+class Node(object):
+    def __init__(self, pos, size):
+        self.pos = pos
+        self.size = size
+        self.bodies = []
+        self.children = []
+        self.mass = 0
+        self.cog = (pos[0] + size/2, pos[1] + size/2)
+
+    def calc_cog(self):
+        self.mass = sum(body.mass for body in self.bodies)
+        if self.mass == 0:
+            return
+        cog_x = sum(body.cog[0] * body.mass for body in self.bodies) / self.mass
+        cog_y = sum(body.cog[1] * body.mass for body in self.bodies) / self.mass
+        self.cog = (cog_x, cog_y)
+
+    def contains(self, body):
+        match_x = body.cog[0] >= self.pos[0] and body.cog[0] < self.pos[0] + self.size
+        match_y = body.cog[1] >= self.pos[1] and body.cog[1] < self.pos[1] + self.size
+        return match_x and match_y
+
+
+class Engine(object):
+
+    def __init__(self, size, phi=0.5, collision_mode='elastic'):
+        self.root_node = Node((0, 0), size)
+        self.phi = phi  # 10
+        self.collision_mode = collision_mode
+
+        self.collision_modes = {
+            'elastic': self.elastic_collision,
+            'inelastic': self.inelastic_collision,
+        }
+
+    def calc_distance(self, pos1, pos2):
+        delta_x = pos1[0] - pos2[0]
+        delta_y = pos1[1] - pos2[1]
+        dist = math.sqrt(delta_x ** 2 + delta_y ** 2)
+        return dist, delta_x, delta_y
+
+    def slice_node(self, node):
+        nw_node = Node(
+            pos=(node.pos[0], node.pos[1] + node.size/2),
+            size=node.size/2
+        )
+        ne_node = Node(
+            pos=(node.pos[0] + node.size/2, node.pos[1] + node.size/2),
+            size=node.size/2
+        )
+        se_node = Node(
+            pos=(node.pos[0] + node.size/2, node.pos[1]),
+            size=node.size/2
+        )
+        sw_node = Node(
+            pos=(node.pos[0], node.pos[1]),
+            size=node.size/2
+        )
+        children = [nw_node, ne_node, se_node, sw_node]
+
+        delbodies = []
+        for body in node.bodies:
+            for child in children:
+                if child.contains(body):
+                    child.bodies.append(body)
+                    break
+            else:
+                # Body does not fit into subnodes
+                delbodies.append(body)
+
+        node.bodies = [b for b in node.bodies if b not in delbodies]
+        return children
+
+    def elastic_collision(self, body1, body2):
+        if body1.collision or body2.collision:
+            return
+        mass_sum = body1.mass + body2.mass
+        b1_vx = ((body1.mass - body2.mass)/mass_sum)*body1.vel[0] + ((2*body2.mass)/mass_sum)*body2.vel[0]
+        b1_vy = ((body1.mass - body2.mass)/mass_sum)*body1.vel[1] + ((2*body2.mass)/mass_sum)*body2.vel[1]
+
+        b2_vx = ((body2.mass - body1.mass)/mass_sum)*body2.vel[0] + ((2*body1.mass)/mass_sum)*body1.vel[0]
+        b2_vy = ((body2.mass - body1.mass)/mass_sum)*body2.vel[1] + ((2*body1.mass)/mass_sum)*body1.vel[1]
+
+        body1.vel = (b1_vx, b1_vy)
+        body2.vel = (b2_vx, b2_vy)
+
+        body1.collision = True
+        body2.collision = True
+
+    def inelastic_collision(self, body1, body2):
+        if body1.remove or body2.remove:
+            # Collision already done
+            return
+        if body1.mass > body2.mass:
+            keep = body1
+            kill = body2
+        else:
+            keep = body2
+            kill = body1
+        kill.remove = True
+        keep.vel = (
+            (keep.vel[0]*keep.mass + kill.vel[0]*kill.mass) / (keep.mass + kill.mass),
+            (keep.vel[1]*keep.mass + kill.vel[1]*kill.mass) / (keep.mass + kill.mass),
+        )
+        keep.mass += kill.mass
+
+    def calc_force(self, body1, body2):
+        dist, delta_x, delta_y = self.calc_distance(body1.cog, body2.cog)
+        both_bodies = isinstance(body1, Body) and isinstance(body2, Body)
+        if isinstance(body2, Body) and body2.remove:
+            # early abort: body2 has been removed!
+            return 0, 0
+        if not dist and not both_bodies:
+            dist = 1
+            return 0, 0
+        if dist <= 2 and both_bodies:
+            self.collision_modes[self.collision_mode](body1, body2)
+            # Collision done
+            return 0, 0
+        force = (body1.mass * body2.mass) / (dist ** 2)
+        force_x = force * delta_x / dist
+        force_y = force * delta_y / dist
+        return force_x, force_y
+
+    def force_traverse(self, body, node):
+
+        if body.remove:
+            # Traverse abort: body has been removed'
+            return
+
+        if len(node.bodies) == 1 and node.bodies[0] != body:
+            force_x, force_y = self.calc_force(body, node.bodies[0])
+            body.next_force_x += force_x
+            body.next_force_y += force_y
+            return
+        else:
+            dist, delta_x, delta_y = self.calc_distance(body.cog, node.cog)
+            if not dist:
+                dist = .5
+            phi = node.size / dist
+            if phi < self.phi:
+                force_x, force_y = self.calc_force(body, node)
+                body.next_force_x += force_x
+                body.next_force_y += force_y
+                return
+            else:
+                for child in node.children:
+                    self.force_traverse(body, child)
+
+    def tick(self):
+        self.init_children(self.root_node)
+        for body in self.root_node.bodies:
+            body.collision = False
+            body.next_force_x = 0
+            body.next_force_y = 0
+            self.force_traverse(body, self.root_node)
+            ax = -body.next_force_x / body.mass
+            ay = -body.next_force_y / body.mass
+            TIMERATIO = .1
+            body.vel = (
+                body.vel[0] + ax * TIMERATIO,
+                body.vel[1] + ay * TIMERATIO
+            )
+            body.cog = (
+                body.cog[0] + body.vel[0] * TIMERATIO,
+                body.cog[1] + body.vel[1] * TIMERATIO
+            )
+
+        self.root_node.bodies = [
+            b for b in self.root_node.bodies if not b.remove
+        ]
+
+    def init_children(self, node):
+        node.calc_cog()
+        if len(node.bodies) <= 1:
+            return
+        node.children = self.slice_node(node)
+        for child in node.children:
+            self.init_children(child)
+
+    def add_body(self, cog, vel, mass):
+        self.root_node.bodies.append(Body(cog, vel, mass))
+
+    def print_children(self, node):
+        print('node %s' % node.__dict__)
+        for child in node.children:
+            self.print_children(child)
+
+    def traverse_node(self, node):
+        yield node
+        for child in node.children:
+            yield from self.traverse_node(child)
+        #print('called!')
---
+++ cygravity/pygame_bh.py
@@ -0,0 +1,158 @@
+import pygame
+from engine_bh import Engine
+import random
+import math
+
+FPS = 100
+
+LEFT = 1  # left mouse button
+MIDDLE = 2
+RIGHT = 3  # ...
+WHEELUP = 4
+WHEELDOWN = 5
+
+RESOLUTION = (1000, 1000)
+
+
+def main():
+
+    pygame.init()
+
+    screen = pygame.display.set_mode(RESOLUTION)
+
+    pygame.display.set_caption("BH_Engine")
+    pygame.mouse.set_visible(1)
+
+    # frame limiter
+    clock = pygame.time.Clock()
+
+    draw_boxes = False
+    engine = Engine(size=1000)
+
+    SUNMASS = 1000000
+
+    def add_bodies():
+        for i in range(125):
+            x = i * 4
+            cog_left = (x, 500)
+            cog_right = (1000 - x, 500)
+            cog_up = (500, x)
+            cog_down = (500, 1000 - x)
+            dist = 500 - x
+            vel_y = math.sqrt(SUNMASS / dist)
+            engine.add_body(
+                cog=cog_left,
+                vel=(0, vel_y),
+                mass=1
+            )
+            engine.add_body(
+                cog=cog_right,
+                vel=(0, -vel_y),
+                mass=1
+            )
+            engine.add_body(
+                cog=cog_up,
+                vel=(-vel_y, 0),
+                mass=1
+            )
+            engine.add_body(
+                cog=cog_down,
+                vel=(vel_y, 0),
+                mass=1
+            )
+
+    add_bodies()
+    engine.add_body(
+        cog=(500, 500),
+        vel=(0, 0),
+        mass=SUNMASS
+    )
+
+    '''
+    for i in range(15):
+        for j in range(15):
+            engine.add_body(
+                cog=(i * 1.1 + 500, j * 1.1 + 500),
+                vel=(0, 0),
+                mass=1
+            )
+    '''
+
+    running = True
+    run_engine = False
+    while running:
+
+        clock.tick(FPS)
+        background = pygame.Surface(RESOLUTION)
+        background.fill((0, 0, 0, 0))
+        background = background.convert_alpha()
+
+        # temporary surface to draw things to
+        display = pygame.Surface(RESOLUTION)
+        display.fill((0, 0, 0, 0))
+        display = display.convert_alpha()
+
+        display.blit(background, (0, 0))
+
+        if run_engine:
+            engine.tick()
+
+        if draw_boxes:
+            for node in engine.traverse_node(engine.root_node):
+                node_surface = pygame.Surface((node.size, node.size))
+                pygame.draw.rect(
+                    node_surface,
+                    (255, 0, 0, 0),
+                    (0, 0, node.size, node.size)
+                )
+                pygame.draw.rect(
+                    node_surface,
+                    (0, 0, 0, 0),
+                    (1, 1, node.size - 2, node.size - 2)
+                )
+                node_surface = node_surface.convert_alpha()
+                display.blit(node_surface, node.pos)
+
+        for body in engine.root_node.bodies:
+            body_surface = pygame.Surface((1, 1))
+            pygame.draw.rect(
+                body_surface,
+                (255, 255, 255, 0),
+                (0, 0, 1, 1)
+            )
+            body_surface = body_surface.convert_alpha()
+            display.blit(body_surface, body.cog)
+
+        screen.blit(display, (0, 0))
+
+        # look for events
+        for event in pygame.event.get():
+
+            # quit game
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
+                add_bodies()
+                print('left mouse buttondown')
+
+            if event.type == pygame.MOUSEBUTTONUP and event.button == LEFT:
+                print('left mouse buttonup')
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == RIGHT:
+                draw_boxes = not draw_boxes
+                print('right mouse button')
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    run_engine = True
+
+                if event.key == pygame.K_DOWN:
+                    run_engine = False
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # run main programm
+    main()
